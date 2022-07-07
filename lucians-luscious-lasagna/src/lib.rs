507d0a2b27a8938d52519251ab7683d9795134da
Robley G <robleyadrian@gmail.com>
2022-07-07 18:35:20 +0300

    add lasagna submission

---
+++ lucians-luscious-lasagna/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn expected_minutes_in_oven() -> i32 {
+    //! Applies to this module
+    40
+}
+
+/// Apply to the next funct
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    expected_minutes_in_oven() - actual_minutes_in_oven
+}
+
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    number_of_layers * 2
+}
+
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+}
