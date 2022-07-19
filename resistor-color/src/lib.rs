78ac91ec49ac2d141fc95c29060b94d94e3af2a7
Robley G <robleyadrian@gmail.com>
2022-07-19 19:59:45 +0300

    add resistor-color submission

---
+++ resistor-color/src/lib.rs
@@ -0,0 +1,39 @@
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
+use std::fmt;
+
+#[repr(usize)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, IntEnum, Sequence)]
+pub enum ResistorColor {
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
+}
+
+impl fmt::Display for ResistorColor {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+pub fn color_to_value(color: ResistorColor) -> usize {
+    color as usize
+}
+
+pub fn value_to_color_string(value: usize) -> String {
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => resistor.to_string(), // format!("{:?}", resistor),
+        Err(_) => "value out of range".to_string(),
+    }
+}
+
+pub fn colors() -> Vec<ResistorColor> {
+    all::<ResistorColor>().collect::<Vec<_>>()
+}
