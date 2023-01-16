79bfde7dca2cb51cdc4cb034d9544a555b28027b
Robley G <robleyadrian@gmail.com>
2023-01-16 20:56:49 +0300

    use NumericalDuration

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,6 +1,7 @@
-use time::{PrimitiveDateTime as DateTime, Duration};
+use time::PrimitiveDateTime as DateTime;
+use time::ext::NumericalDuration;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(Duration::new(1_000_000_000, 0))
+    start.saturating_add(1e9.seconds())
 }
